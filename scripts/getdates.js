b5770b476d97bedf4fab9b745dfb41406774a672
derasmuso <drasmuson@byupathway.edu>
2025-09-05 22:05:40 -0600

    Home Page Added

---
+++ scripts/getdates.js
@@ -0,0 +1,10 @@
+const currentYear = document.querySelector("#currentyear");
+const today = new Date();
+currentYear.innerHTML = today.getFullYear();
+
+lastModified = document.lastModified;
+document.getElementById("lastModified").innerHTML = "Last Modified: " + lastModified;
+
+
+
+
