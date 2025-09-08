50a0507053c2402719e9be1c7b109b30303280c7
derasmuso <drasmuson@byupathway.edu>
2025-09-08 12:47:28 -0600

    Document object model activity added

---
+++ week02/scripts/bom.js
@@ -0,0 +1,11 @@
+const input = document.querySelector('#favchap');
+const button = document.querySelector('button');
+const list = document.querySelector('______');
+
+const li = document.createElement('li');
+const deletebutton = document.createElement('button');
+
+li.textContent = input.value;
+deletebutton.textContent = '❌';
+li.append(deletebutton);
+list.append(li);
