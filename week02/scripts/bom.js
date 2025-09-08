c6ebd97e435c2a6f7e4b6a60a279c77ede5830b2
derasmuso <drasmuson@byupathway.edu>
2025-09-08 16:39:09 -0600

    bom.js updated

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -1,11 +1,37 @@
 const input = document.querySelector('#favchap');
 const button = document.querySelector('button');
-const list = document.querySelector('______');
+const list = document.querySelector('#list');
 
-const li = document.createElement('li');
-const deletebutton = document.createElement('button');
+//const li = document.createElement('li');
+//const deletebutton = document.createElement('button');
 
-li.textContent = input.value;
-deletebutton.textContent = '❌';
-li.append(deletebutton);
-list.append(li);
+//li.textContent = input.value;
+//deletebutton.textContent = '❌';
+//li.append(deletebutton);
+//list.append(li);
+
+button.addEventListener('click', function () {
+    if (input.value.trim() !== '') {
+        const li = document.createElement('li');
+        const deletebutton = document.createElement('button');
+
+        li.textContent = input.value;
+        deletebutton.textContent = '❌';
+        li.append(deletebutton);
+        list.append(li);
+
+        deletebutton.addEventListener('click', function () {
+            list.removeChild(li);
+            input.focus();
+
+
+        });
+        input.value = '';
+    }
+    input.focus();
+});
+
+
+
+//input.value = '';
+//input.focus();
