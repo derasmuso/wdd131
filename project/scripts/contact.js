84c7ea6a1322fa6adfb06e5a03b7ef7e113cd14e
derasmuso <drasmuson@byupathway.edu>
2025-10-09 16:23:10 -0600

    localstorage added to contact.js

--- project/scripts/contact.js
+++ project/scripts/contact.js
@@ -28,3 +28,23 @@ subjects.forEach(subject => {
     option.textContent = subject.name;
     subjectSelect.appendChild(option);
 });
+
+// When the form is first filled out, this uses localstorage to save the users input for first name, last name, and email.
+// The user will only have to select the message sbuject and enter in their message.
+
+document.addEventListener("DOMContentLoaded", () => {
+    const firstInput = document.getElementById("userFirst");
+    const lastInput = document.getElementById("userLast");
+    const emailInput = document.getElementById("userEmail");
+    const form = document.querySelector("form");
+
+    firstInput.value = localStorage.getItem("firstName") || "";
+    lastInput.value = localStorage.getItem("lastName") || "";
+    emailInput.value = localStorage.getItem("email") || "";
+
+    form.addEventListener("submit", () => {
+        localStorage.setItem("firstName", firstInput.value);
+        localStorage.setItem("lastName", lastInput.value);
+        localStorage.setItem("email", emailInput.value);
+    })
+});
